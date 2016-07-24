21f5d883c5c0f10719fdd76df57002adab31fd89
Andy Esser <aesser@zerodependency.co.uk>
2016-07-24 12:51:31 +0100

    Whut...

--- include/siggen.hpp
+++ include/siggen.hpp
@@ -3,7 +3,18 @@
 #include <iostream>
 #include <string.h>
 #include <math.h>
+#include <stdint.h>
 
 #include "ao/ao.h"
 
 #define BUF_SIZE 4096
+
+ao_device *device = 0;
+ao_sample_format format;
+int driver = 0;
+
+int16_t* buffer = 0;
+int buf_size = 0;
+
+int16_t* bufferStop = 0;
+int buf_size_stop = 0;
--- src/siggen.cpp
+++ src/siggen.cpp
@@ -1,56 +1,57 @@
 #include "siggen.hpp"
 
 //std::string sentence = "$$MIKEL-3,1469352038,Flight7,02:19:59,37.3466186523,-121.946205139,32.0,0.0,3.95110321045,0.106474421918,0.420691430569,0.804084181786,0.0,0.0,24.1094760895,100874.882812,32.3646697998,1.39783334732,43*ED19";
-std::string sentence = "$$EAD-PLS-001";
+//std::string sentence = "$$EAD-PLS-001";
+
+char message[] = {"\0\0\0\0\0"};
 
 float frequencies[] = {1500.0, 2000.0};
+float freqL = 1500.0f;
+float freqH = 2000.0f;
 
-void populateBuffer(char* buf, int startIndex, int length, int bit) {
-	int sample = 0;
+float freq[] = {
+	freqL * M_PI * 2.0f,
+	freqH * M_PI * 2.0f
+};
 
-	for (unsigned int i = startIndex; i < (startIndex + length); i++) {
-		sample = (int)(2.0 * 32768.0 * sin(2 * M_PI * frequencies[bit] * ((float) i / length)));
-		
-		buf[4 * i] = sample & 0xff;
-		buf[4 * i+1] = (sample >> 8) & 0xff;
-		buf[4 * i+2] = sample & 0xff;
-		buf[4 * i+3] = (sample >> 8) & 0xff;
+float phase = 0.0f;
+int amplitude = 32768;
+
+int newFunction(int16_t* buffer, int index, int samplesPerBit, int bit) {
+	float delta = freq[bit] / 44100; 
+
+	for (unsigned int s = 0; s < samplesPerBit; s++) {
+		buffer[index] = cos(phase) * amplitude;
+		phase += delta;
+		index++;
 	}
+
+	return index;
 }
 
-int main(int argc, char* argv[]) {
 
-	ao_device *device;
-	ao_sample_format format;
-	int driver;
-	char* buffer;
-	int buf_size;
+int main(int argc, char* argv[]) {
 
 	std::cout << "libao example program" << std::endl;
 
 	ao_initialize();
-
-	int count = 0;
-	ao_info** driver_info = ao_driver_info_list(&count);
-
-	for(unsigned int d = 0; d < count; d++) {
-		ao_info* info = driver_info[d];
-		std::cout << d << ": [" << info->type << "] - " << info->short_name << std::endl;
-	}
-
-	std::cout << "Driver Count: " << count << std::endl;
-
 	driver = ao_default_driver_id();
 
 	memset(&format, 0, sizeof(format));
 	format.bits = 16;
-	format.channels = 2;
+	format.channels = 1;
 	format.rate = 44100;
 	format.byte_format = AO_FMT_LITTLE;
 
+	device = ao_open_live(driver, &format, NULL);
+
+	if (device == NULL) {
+		std::cerr << "Error Opening Device" << std::endl;
+		return -1;
+	}
 
 	// RTTY Info
-	int len = sentence.size();
+	int len = 5;//sentence.size();
 	std::cout << "Byte Count: " << len << std::endl;
 	
 	int startBitCount = 1;
@@ -70,30 +71,28 @@ int main(int argc, char* argv[]) {
 	int bufferSize = sentenceLengthMilliseconds * format.rate;
 	// End RTTY Info
 
-	device = ao_open_live(driver, &format, NULL);
-
-	if (device == NULL) {
-		std::cerr << "Error Opening Device" << std::endl;
-		return -1;
-	}
-
 	buf_size = format.bits / 8 * format.channels * (format.rate * (sentenceLengthMilliseconds / 1000));
-	buffer = (char*) calloc(buf_size, sizeof(char));
+	buffer = (int16_t*) calloc(buf_size, sizeof(int16_t));
 
 	int samplesPerBit = (format.rate / 1000) * milliSecondsSymbol;
 	int index = 0;
 
 	for(unsigned int c = 0; c < len; c++) {
-		char ch = sentence[c];
+		char ch = message[c];
+
+		index = newFunction(buffer, index, samplesPerBit, 0);
 		for (unsigned int b = 0; b < asciiMode; b++) {
 			int bit = (c >> b) & 0x01;
 
-			populateBuffer(buffer, index, samplesPerBit, bit);
-			index += samplesPerBit;
+			index = newFunction(buffer, index, samplesPerBit, bit);
 		}
+		index = newFunction(buffer, index, samplesPerBit, 1);
+		index = newFunction(buffer, index, samplesPerBit, 2);
 	}
 
-	ao_play(device, buffer, buf_size);
+	while (true) {
+		ao_play(device, (char*)buffer, buf_size);
+	}
 	
 	ao_close(device);
 	ao_shutdown();
